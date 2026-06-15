0e705d28abb868f6417537024fa8ea24d3220c41
falkTX <falktx@falktx.com>
2026-06-15 09:42:59 +0200

    Setup some more metadata
    Signed-off-by: falkTX <falktx@falktx.com>

--- src/DistrhoPluginInfo.h
+++ src/DistrhoPluginInfo.h
@@ -6,12 +6,19 @@
 
 #include "config.h"
 
+#define DISTRHO_PLUGIN_BRAND "Libre Audio"
+#define DISTRHO_PLUGIN_BRAND_ID LiAu
 #define DISTRHO_PLUGIN_HOMEPAGE "https://libreaudio.org/"
 #define DISTRHO_PLUGIN_LICENSE "GPL-3.0-or-later"
 #define DISTRHO_PLUGIN_MAKER "Libre Audio"
 
 // TODO
-#define DISTRHO_PLUGIN_HAS_UI 0
+#define DISTRHO_PLUGIN_HAS_UI      0
+#define DISTRHO_PLUGIN_IS_RT_SAFE  1
+
+#define DISTRHO_UI_FILE_BROWSER    0
+#define DISTRHO_UI_USER_RESIZABLE  0
+#define DISTRHO_UI_USE_NANOVG      1
 
 #if DISTRHO_PLUGIN_NUM_INPUTS != 2
 #error DISTRHO_PLUGIN_NUM_INPUTS != 2
