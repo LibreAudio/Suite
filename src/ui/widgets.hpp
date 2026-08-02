16f6cd52663e0650ee84ac8d8c444a2a61f30a51
falkTX <falktx@falktx.com>
2026-08-02 09:29:15 +0200

    Fix plugin name
    Signed-off-by: falkTX <falktx@falktx.com>

--- src/ui/widgets.hpp
+++ src/ui/widgets.hpp
@@ -81,12 +81,16 @@ public:
     LibreAudioPluginName(NanoSubWidget* const parent)
         : LibreAudioWidget(parent)
     {
-        fText.toUpper();
+        std::memcpy(fName, DISTRHO_PLUGIN_NAME + 2, sizeof(DISTRHO_PLUGIN_NAME) - 2);
+        fName[sizeof(DISTRHO_PLUGIN_NAME) - 3] = '\0';
+
+        for (uint i = 0; i < sizeof(DISTRHO_PLUGIN_NAME) - 2; ++i)
+            fName[i] = std::toupper(fName[i]);
 
         Rectangle<float> bounds;
         textAlign(0);
         textLetterSpacing(Metrics::TopBar::PluginName::letterSpacing * fScaleFactor);
-        textBounds(0, 0, fText, nullptr, bounds);
+        textBounds(0, 0, fName, nullptr, bounds);
         setWidth(bounds.getWidth());
     }
 
@@ -97,11 +101,11 @@ protected:
         fontSize(Metrics::fontSize * fScaleFactor);
         textAlign(ALIGN_CENTER | ALIGN_MIDDLE);
         textLetterSpacing(Metrics::TopBar::PluginName::letterSpacing * fScaleFactor);
-        text(getWidth() * 0.5f, getHeight() * 0.5f, fText);
+        text(getWidth() * 0.5f, getHeight() * 0.5f, fName);
     }
 
 private:
-    String fText { DISTRHO_PLUGIN_SHORTNAME };
+    char fName[sizeof(DISTRHO_PLUGIN_NAME) - 2];
 };
 
 // --------------------------------------------------------------------------------------------------------------------
